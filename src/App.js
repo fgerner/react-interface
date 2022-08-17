87f70ecc7fcbbfa35d431f1dd48d6abb311586d7
fgerner <fredrik.gerner@qantas.com.au>
2022-08-17 21:24:58 +1000

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import {BiArchive} from 'react-icons/bi';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Your Appointments</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className={'text-3xl font-bold'}><BiArchive className={'inline-block'}/>Your Appointments</h1>
+        </div>
+    );
 }
 
 export default App;
