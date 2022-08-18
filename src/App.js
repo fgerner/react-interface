6887ff3d8ebdc5fb2afbe40f4ffa0b99b8312ef4
fgerner <fredrik.gerner@qantas.com.au>
2022-08-18 14:36:22 +1000

    added styling

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import {BiArchive} from 'react-icons/bi';
 
 function App() {
     return (
-        <div className="App">
-            <h1 className={'text-3xl font-bold'}><BiArchive className={'inline-block'}/>Your Appointments</h1>
+        <div className="App container mx-auto mt-3 font-thin">
+            <h1 className={'text-5xl'}>
+                <BiArchive className={'inline-block text-red-400 align-top'}/>
+                Your Appointments</h1>
         </div>
     );
 }
